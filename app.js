cf58188ddc47a4aff33b11ad1f1c9663e5603545
tac <tacccccc21@gmail.com>
2020-10-23 16:27:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,42 @@ const answers = ['スーパーファミコン','プレイステーション２',
 const correct = 'ニンテンドーDS';
 
 document.getElementById('js-question').textContent = question;
+
+const $button = document.getElementsByTagName('button')
+
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
+
+$button[0].addEventListener('click', ()=>{
+  if(correct === $button[0].textContent){
+  window.alert('正解！')
+  }else{
+  window.alert('残念！');
+  }
+});
+
+$button[1].addEventListener('click', ()=>{
+  if(correct === $button[1].textContent){
+  window.alert('正解！')
+  }else{
+  window.alert('残念！');
+  }
+});
+
+$button[2].addEventListener('click', ()=>{
+  if(correct === $button[2].textContent){
+  window.alert('正解！')
+  }else{
+  window.alert('残念！');
+  }
+});
+
+$button[3].addEventListener('click', ()=>{
+  if(correct === $button[3].textContent){
+  window.alert('正解！')
+  }else{
+  window.alert('残念！');
+  }
+});
