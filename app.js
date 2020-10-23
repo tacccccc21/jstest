ed422f2c0577e034f381804aaafc8d155a76015c
tac <tacccccc21@gmail.com>
2020-10-23 16:07:19 +0900

    firstquestion

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ const question = 'ゲーム史上、最も売れたゲーム機は？';
 const answers = ['スーパーファミコン','プレイステーション２','ニンテンドースイッチ','ニンテンドーDS'];
 
 const correct = 'ニンテンドーDS';
+
+document.getElementById('js-question').textContent = question;
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let haruhi = 'Hello World!';
+// let haruhi = 'Hello World!';
 
 // haruhi = 'aaaaaaaaaaa';
 
-const kyon = 'lsllslslsls';
+// const kyon = 'lsllslslsls';
 
-let inoki = ['1fd','2fd','3fd']
+// let inoki = ['1fd','2fd','3fd']
 
-console.log(inoki[0]);
+// console.log(inoki[0]);
 
   // let index = 0;
   // while(index < inoki.length){
@@ -21,27 +21,27 @@ console.log(inoki[0]);
 // }
 
 //　関数
-const test = (arg) => {
-  if(inoki.length > arg){
-    console.log('ボンバイエ');
-  }else{
-    console.log('aasasaasas');
-  }
-};
+// const test = (arg) => {
+//   if(inoki.length > arg){
+//     console.log('ボンバイエ');
+//   }else{
+//     console.log('aasasaasas');
+//   }
+// };
 
-test(4);
+// test(4);
 
 
 // object
-const unko2 = {
-  color: 'pink',
-  size: 'large',
-  purfume: 'mint',
-  goToilet: () => {
-    console.log('hello');
-  }
-};
+// const unko2 = {
+//   color: 'pink',
+//   size: 'large',
+//   purfume: 'mint',
+//   goToilet: () => {
+//     console.log('hello');
+//   }
+// };
 
-console.log(document.getElementsByTagName('button')[0].addEventListener('click', ()=> {
-  window.alert('yeah ha');
-}));
+// console.log(document.getElementsByTagName('button')[0].addEventListener('click', ()=> {
+//   window.alert('yeah ha');
+// }));
