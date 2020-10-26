d9927f52bd7a69e2b8ebcff2489cd73914433bdf
tac <tacccccc21@gmail.com>
2020-10-26 20:51:26 +0900

    e.target

--- app.js
+++ app.js
@@ -4,49 +4,56 @@ const answers = ['スーパーファミコン','プレイステーション２',
 
 const correct = 'ニンテンドーDS';
 
-document.getElementById('js-question').textContent = question;
-
 const $button = document.getElementsByTagName('button')
+
+
+//クイズの問題文、選択肢を定義
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = question;
+  let buttonIndex = 0;
+  let buttonLength = $button.length;
+  while(buttonIndex < buttonLength){
+  $button[buttonIndex].textContent = answers[buttonIndex];
+  buttonIndex++;
+}
+}
+
+setupQuiz();
 
 // $button[0].textContent = answers[0];
 // $button[1].textContent = answers[1];
 // $button[2].textContent = answers[2];
 // $button[3].textContent = answers[3];
 
-let buttonIndex = 0;
-let buttonLength = $button.length;
-while(buttonIndex < $button.length){
-  $button[buttonIndex].textContent = answers[buttonIndex];
-  buttonLength++;
-}
+
 
 //正誤判定
-$button[0].addEventListener('click', ()=>{
-  if(correct === $button[0].textContent){
+$button[0].addEventListener('click', (e)=>{
+  if(correct === e.target.textContent){
   window.alert('正解！')
   }else{
   window.alert('残念！');
   }
 });
 
-$button[1].addEventListener('click', ()=>{
-  if(correct === $button[1].textContent){
+$button[1].addEventListener('click', (e)=>{
+  if(correct === e.target.textContent){
   window.alert('正解！')
   }else{
   window.alert('残念！');
   }
 });
 
-$button[2].addEventListener('click', ()=>{
-  if(correct === $button[2].textContent){
+$button[2].addEventListener('click', (e)=>{
+  if(correct === e.target.textContent){
   window.alert('正解！')
   }else{
   window.alert('残念！');
   }
 });
 
-$button[3].addEventListener('click', ()=>{
-  if(correct === $button[3].textContent){
+$button[3].addEventListener('click', (e)=>{
+  if(correct === e.target.textContent){
   window.alert('正解！')
   }else{
   window.alert('残念！');
